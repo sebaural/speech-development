0ed641ce3bebaa04b73ad2495caf68b4562d08a8
ural <sevomax@gmail.com>
2025-10-16 19:40:42 -0400

    site's logo update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="border-t bg-white">
       <div className="mx-auto max-w-6xl px-6 py-6 grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className="flex">
-          <div className="font-semibold"><img src="/images/РИР_лого.png" alt="Речь и Развитие" className="h-16 md:h-12 lg:h-19 w-auto" /></div>
+          <div className="font-semibold"><img src="/images/logo_new.png" alt="Речь и Развитие" className="h-8 md:h-12 lg:h-19 w-auto" /></div>
           <p className="mt-3 text-sm text-slate-600">© {new Date().getFullYear()} Речь и Развитие. Все права защищены.</p>
         </div>
         <div className="flex items-center text-sm text-slate-600">
