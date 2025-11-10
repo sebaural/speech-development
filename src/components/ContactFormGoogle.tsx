c288c3baa8c368c462e33dad81010b63332a4add
ural <sevomax@gmail.com>
2025-11-10 05:46:38 -0500

    playing with google form

---
+++ src/components/ContactFormGoogle.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function ContactForm() {
+  return (
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSe9wlwuvUghKwfSulIL4u5fY98POJvvD2Ki9sfaE7RfGw7ITg/viewform?embedded=true"
+      width="840"
+      height="486"
+      frameBorder="0"
+      marginHeight={0}
+      marginWidth={0}
+      style={{ width: '100%', maxWidth: '100%', border: 0 }}
+      title="Контактная форма"
+    >
+      Loading…
+    </iframe>
+  )
+}
